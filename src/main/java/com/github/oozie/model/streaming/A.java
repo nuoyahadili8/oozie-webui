3521982a25ab09897c12434b4e6304cd8762a120
Liang An <liang.an@teradata.com>
2018-12-19 09:35:55 +0800

    readme.m更新

---
+++ src/main/java/com/github/oozie/model/streaming/A.java
@@ -0,0 +1,13 @@
+package com.github.oozie.model.streaming;
+
+/**
+ * @Project:
+ * @Description:
+ * @Version 1.0.0
+ * @Throws SystemException:
+ * @Author: <li>2018/12/19/019 Administrator Create 1.0
+ * @Copyright ©2018-2019 al.github
+ * @Modified By:
+ */
+public class A {
+}
